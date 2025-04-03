afa8be47a1704ba5f9b3572fa0915ce6ac4666cb
Vince Foley <vince@felt.com>
2025-04-03 14:55:52 -0700

    Init

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Errors
+# components.schemas.LayerMetadata.default=`not_provided` is not of type `date`
+#
+
+
+# docker run --rm openapitools/openapi-generator-cli generate --help
+
+docker run --rm -v "/Users/vince/felt/felt_api:/local" -v "/Users/vince/felt/felt:/felt" openapitools/openapi-generator-cli generate \
+          -i /felt/priv/felt_openapi_spec_v2.json \
+          --skip-validate-spec \
+          --package-name felt_api \
+          -g python \
+          -o /local
